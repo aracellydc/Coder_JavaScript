76a1cf11ba7e99f9c36e38da6cbcf99ca91a4d1d
aracellydc <aracelly.carmona@unmsm.edu.pe>
2023-05-16 21:43:41 -0500

    SegundaPreentrega

--- main.js
+++ main.js
@@ -4,51 +4,51 @@ let usuario = prompt("Ingresa tu nombre: ")
 
 alert(usuario + ", ¿Deseas ver que frutas tenemos disponibles?")
 
-alert("Hola " + usuario + " ,tenemos estas frutas junto a sus precios: \n\n1. Manzanas (1kg = s/4)\n2. Naranjas (1kg = s/3)\n3. Plátano (1kg = s/6)\n4. Papaya (1kg = s/4)\n\nPara continuar apreta en 'Aceptar'")
 
-
-let PrecioManz = 4
-let PrecioNara = 3
-let PrecioPlat = 6
-let PrecioPapa = 4
-
-
-function kilosFruta(fruta, precio){
-    let kilos =0
-    let compra = true
-    do{
-        kilos = parseInt(prompt("¿Cuantos kilos desea llevar de " + fruta + " ?"))
-        if (kilos === null){
-            break
-        }else if (kilos>=0){
-            PrecioFinal = precio * kilos
-            break 
-        }else{
-            alert("No se ingreso cantidad correcta de kilos")
-        }
-    }
-    while(compra)
-    return kilos
+const Frutas = function(nombre, precio){
+    this.nombre= nombre,
+    this.precio = precio
 }
 
-let KilosManz = kilosFruta("Manzanas",PrecioManz)
-let precioFinalManz = KilosManz * PrecioManz
-
-let KilosNara = kilosFruta("Naranjas",PrecioNara)
-let precioFinalNara = KilosNara * PrecioNara
-
-let KilosPlat = kilosFruta("Plátano",PrecioPlat)
-let precioFinalPlat = KilosPlat * PrecioPlat
-
-let KilosPapa = kilosFruta("Papaya",PrecioPapa)
-let precioFinalPapa = KilosPapa * PrecioPapa
-
-let PrecioFinalCompra = precioFinalManz + precioFinalNara + precioFinalPlat + precioFinalPapa
-alert("El resumen de la compra es: \n" + KilosManz+ " Kilos de Manzanas: s/"+ precioFinalManz+ "\n" +KilosNara+ " Kilos de Naranjas: s/"+ precioFinalNara + "\n" +KilosPlat+ " Kilos de Plátanos: s/"+ precioFinalPlat + "\n" +KilosPapa+ " Kilos de Papaya: s/"+ precioFinalPapa)
-alert("El monto final de su compra es: s/" + PrecioFinalCompra)
+let fruta1 = new Frutas("Manzanas",4)
+let fruta2 = new Frutas("Naranjas",3)
+let fruta3 = new Frutas("Plátano",6)
+let fruta4 = new Frutas("Papaya",4)
 
 
+let lista = [fruta1, fruta2,fruta3, fruta4]
+
+function BuscarFruta(){
+    let ingresa = prompt("Ingresa la fruta que deseas comprar").trim().toLowerCase()
+    let resultado = lista.filter((Frutas) => Frutas.nombre.toLowerCase().includes(ingresa))
+
+    if(resultado.length>0){
+        console.table(resultado)
+    }else{
+        alert("No se encontro ninguna coincidencia con: "+ ingresa)
+    }
+}
+console.table(lista)
+
+function agregarFruta(){
+    let nombre = prompt("Ingresa la fruta que nos falta en nuestra variedad:").trim();
+    let precio = parseInt(prompt("Ingresa el precio que pagarias por esta fruta:"));
+
+    if (isNaN(precio) || nombre ==="" ){
+        alert("Ingresa valores válidos.")
+        return;
+    }
+    let fruta = new Frutas(nombre, precio);
 
 
+    if(lista.some((elemento) => elemento.nombre === fruta.nombre)){
+        alert("El producto ya existe en nuestra tienda.");
+        return;
+    }
+
+    lista.push(fruta);
+
+    console.table(lista)
+}
 
 
